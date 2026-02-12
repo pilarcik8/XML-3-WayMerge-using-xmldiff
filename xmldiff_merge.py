7a43086ff1df604ec24369e81da604d1ff5a670d
Stick <sompilarcikmichal@gmail.com>
2026-02-12 17:07:28 +0100

    Teraz bude hladat v priečinku 'inputs' vetvi. Ak nenajde iteraciu vetvi končí. Iteruje od čísla 0.

--- xmldiff_merge.py
+++ xmldiff_merge.py
@@ -13,9 +13,13 @@ def save_xml(tree, path):
     tree.write(full, pretty_print=True, encoding="utf-8", xml_declaration=True)
 
 def merge_three_way(base_path, left_path, right_path, result_path):
-    base_tree = load_xml(base_path)
-    left_tree = load_xml(left_path)
-    right_tree = load_xml(right_path)
+    try:
+        base_tree = load_xml(base_path)
+        left_tree = load_xml(left_path)
+        right_tree = load_xml(right_path)
+    except Exception as e: 
+        return False
+
 
     diff_left = main.diff_trees(base_tree, left_tree)
     diff_right = main.diff_trees(base_tree, right_tree)
@@ -25,31 +29,40 @@ def merge_three_way(base_path, left_path, right_path, result_path):
     merged_root = patcher.patch(diff_right, merged_root)
 
     save_xml(etree.ElementTree(merged_root), result_path)
-"""
-if __name__ == "__main__":
-    merge_three_way(
-        base_path="base_class.xml",
-        left_path="left_class.xml",
-        right_path="right_class.xml",
-        result_path="result_class.xml"
-    )
-"""
-
-"""
-if __name__ == "__main__":
-    merge_three_way(
-        base_path="base_set.xml",
-        left_path="left_set.xml",
-        right_path="right_set.xml",
-        result_path="result_set.xml"
-    )
-"""
+    return True
 
 
 if __name__ == "__main__":
-    merge_three_way(
-        base_path="base_list.xml",
-        left_path="left_list.xml",
-        right_path="right_list.xml",
-        result_path="result_list.xml"
-    )
+    index = 0
+
+    inputs_dir = "inputs"
+    result_dir= "results"
+
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
+
+    if not os.path.exists(inputs_dir): 
+        print(f"Nexistoval priečinok '{inputs_dir}' v projekte. Zaplnite ho súbormi a spustite program znovu.") 
+        os.mkdir(inputs_dir)
+        exit(1)
+
+    base_path=inputs_dir + "/" + str(index) + "/base"+ str(index) +".xml"
+    left_path=inputs_dir + "/" + str(index) + "/left"+ str(index) +".xml"
+    right_path=inputs_dir + "/" + str(index) + "/right"+ str(index) +".xml"
+    
+    result_path=result_dir + "/result"+ str(index) +".xml"
+    
+    while merge_three_way(
+        base_path=base_path,
+        left_path=left_path,
+        right_path=right_path,
+        result_path=result_path
+    ):
+        index += 1
+        base_path=inputs_dir + "/"+ str(index) +"/base"+ str(index) +".xml"
+        left_path=inputs_dir + "/"+ str(index) +"/left"+ str(index) +".xml"
+        right_path=inputs_dir + "/"+ str(index) +"/right"+ str(index) +".xml"
+        result_path=result_dir + "/result"+ str(index) +".xml"
+    
+    if index == 0:print("Nenajdeny ziadny súbor na spracovanie.")
+    else: print(f"Zpracované {index} súborov/súbory.")
